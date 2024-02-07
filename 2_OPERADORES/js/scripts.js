6564865d10ce0f2e2071b78bc7cacef618330040
notShal <shaldanillo@gmail.com>
2024-02-07 18:48:06 -0300

    aula 1

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,61 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - Operações aritiméticas
+console.log(3 + 5);
+console.log(10 - 5);
+console.log(5 * 5);
+console.log(10 / 2);
+console.log(5 + (4 * 2));
+
+// 3 - Special Numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Um Texto");
+console.log('Mais um Texto');
+console.log(`13`);
+
+// 5 - Simbolos especiais em string
+console.log("Testando a \n quebra de linha");
+
+console.log("Espaçamento \t de tab");
+
+// 6 - Concatenação
+console.log("Oi," + " tudo" + " bem?");
+console.log(`Testando` + ` com` + ` crase.`);
+
+// 7 - Interpolação
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+console.log(`Podemos executar qualquer coisa aqui ${(console.log("teste"))}`);
+
+// 8 - Booleans
+console.log(true);
+console.log(5 > 20);
+console.log(30 > 10);
+console.log(typeof false);
+
+// 9 - Comparações
+console.log(5 <= 5);
+console.log(5 < 5);
+console.log(10 == 10);
+console.log(10 == 9);
+
+// 10 - Idênticos
+console.log(9 == "9"); // valor igual
+console.log(9 === "9"); // tipo diferente
+console.log(9 != "9"); // diferente
+console.log(9 !== "9"); // tipo
+
+// OBS - Operadores lógicos (&& - AND / || - OR / ! - NOT)
+
+//
