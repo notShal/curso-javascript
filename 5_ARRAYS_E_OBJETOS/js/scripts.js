fbebd457aca0baf6928a40f0f2e09dda42319337
notShal <shaldanillo@gmail.com>
2024-02-21 12:43:12 -0300

    JSON

--- 5_ARRAYS_E_OBJETOS/js/scripts.js
+++ 5_ARRAYS_E_OBJETOS/js/scripts.js
@@ -367,3 +367,37 @@ const myList = ["Avião", "Submariono", "Carro", "Trator"];
 const [veiculoA, veiculoB, veiculoC] = myList;
 
 console.log(veiculoA, veiculoB, veiculoC);
+
+// 27 - JSON
+
+const myJson = '{"name": "Shal", "age": 20, "skills": ["PHP", "JavaScript", "Python"]}';
+
+console.log(myJson);
+
+console.log(typeof myJson);
+
+// 28 - JSON para objeto e objeto para JSON
+
+const myObject = JSON.parse(myJson);
+
+console.log(myObject);
+
+console.log(myObject.name);
+
+console.log(typeof myObject);
+
+// JSON invalido
+
+const badJson = '{"name": Shal, "age": 20}';
+
+// const myBadObject = JSON.parse(badJson);
+
+myObject.isOpenToWork = true;
+
+console.log(myObject);
+
+const myNewJson = JSON.stringify(myObject);
+
+console.log(myNewJson);
+
+console.log(typeof myNewJson);
