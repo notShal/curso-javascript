c0ca4d5908404fa5a62b354dea17ddea43eb82d8
notShal <shaldanillo@gmail.com>
2024-02-22 12:43:06 -0300

    métodos oop

---
+++ 6_OOP/js/scripts.js
@@ -0,0 +1,20 @@
+// 1 - métodos 
+
+const animal = {
+    nome: "Bob",
+    latir: function(){
+        console.log("Au au");
+}
+};
+
+console.log(animal.nome);
+
+animal.latir();
+
+// 2 - aprofundando em métodos
+
+const pessoa = {
+
+    nome: "Shal"
+}
+
