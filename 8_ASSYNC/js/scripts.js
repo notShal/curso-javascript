8eb6f0a0e764e8a4098a3df86098fc91ef56ac96
notShal <shaldanillo@gmail.com>
2024-02-24 11:32:08 -0300

    programacao assincrona

--- 8_ASSYNC/js/scripts.js
+++ 8_ASSYNC/js/scripts.js
@@ -1,31 +1,129 @@
-// 1 - setTimeout
+// // 1 - setTimeout
 
-console.log("Ainda não executou");
+// console.log("Ainda não executou");
 
-setTimeout(function() {
-    console.log("Requisição assícrona");
-}, 2000)
+// setTimeout(function() {
+//     console.log("Requisição assícrona");
+// }, 2000)
 
-console.log("ainda não executou 2");
+// console.log("ainda não executou 2");
 
-// 2 - setInsterval
+// // 2 - setInsterval
 
-console.log("Ainda não começou");
+// console.log("Ainda não começou");
 
-// setInterval(function() {
-//     console.log("Intervalo assícrono");
-// }, 3000);
+// // setInterval(function() {
+// //     console.log("Intervalo assícrono");
+// // }, 3000);
 
-console.log("Ainda não começou 2");
+// console.log("Ainda não começou 2");
 
-// 3 - promises
+// // 3 - promises
 
-const promessa = Promise.resolve(5 + 5)
+// const promessa = Promise.resolve(5 + 5)
 
-console.log("Algum código");
+// console.log("Algum código");
 
-promessa.then(value => {
-    console.log(`A soma é ${value}`);
-})
+// promessa.then(value => {
+//     console.log(`A soma é ${value}`);
+//     return value;
+// })
 
-console.log("Outro código");
+// .then((value) => value - 1)
+// .then((value) => console.log(`Agora é ${value}`))
+
+// console.log("Outro código");
+
+// // 4 - falha na promise
+// Promise.resolve(4 * "asd")
+//     .then((n) => {
+//         if(Number.isNaN(n)) {
+//             throw new Error("Valores inválidos");
+//         }
+//     })
+//     .catch((err) => console.log(`Um error ocorreu: ${err}`));
+
+// 5 -rejeição 
+
+// function checkNumber(n) {
+//     return new Promise((resolve, reject) => {
+
+//         if (n > 10) {
+//             resolve("O número é maior que 10")
+//         } else {
+//             reject(new Error("Número muito baixo"));
+//         }
+//     });
+// }
+
+// const a = checkNumber(20)
+// const b = checkNumber(10)
+
+// a.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log(`um error ocorreu: ${err}`)
+// );
+
+// b.then((v) => console.log(`O resultado é ${v}`)).catch((err) => console.log(`um error ocorreu: ${err}`)
+// );
+
+// 6 - resolvendo varias promises
+const p1 = new Promise((resolve, reject) =>{
+    setTimeout(function() {
+        resolve(10);
+    }, 3000);
+});
+
+const p2 = Promise.resolve(10 + 10)
+
+const p3 = new Promise((resolve, reject) =>{
+    if (30 > 10) {
+        resolve(30);
+    }else {
+        reject("Error!");
+    }
+});
+
+Promise.all([p1, p2, p3]).then((values) => console.log(values));
+
+// 7 - async functions
+
+async function somarComDalay(a, b) {
+    return a + b
+}
+
+somarComDalay(2, 4).then((value) => {
+    console.log(`O valor da soma é: ${value}`);
+});
+
+console.log("test async");
+
+// 8 - async await
+
+function resolveComDelay() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve("Resolveu a Promise");
+        }, 2000);
+    });
+}
+
+async function chamadaAsync() {
+    console.log("Chamando a Promise, e esperando o resultado");
+    const result = await resolveComDelay();
+    console.log(`O resultado chegou: ${result}`);
+}
+
+chamadaAsync();
+
+// 9 - generators
+
+function* generator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = generator()
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
