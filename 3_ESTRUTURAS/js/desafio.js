dee7c3a5317ee34d9f9e2848e0f0eb8b75e2358d
notShal <shaldanillo@gmail.com>
2024-02-26 09:17:39 -0300

    if else

---
+++ 3_ESTRUTURAS/js/desafio.js
@@ -0,0 +1,29 @@
+// DESAFIO - Ultilizando If / Else, crie um codigo que:
+//      - verifica se uma cidade é grande ou pequena
+//      - Cidade grande possui mais de 300mil habitantes
+
+const saoPaulo = 11000000
+const brasilia = 2800000
+const engenhoVelho = 1350
+const serraDaSaudade = 833
+
+if (engenhoVelho >= 3000) {
+    console.log(`São Paulo  é uma grande cidade!`);
+} else {
+    console.log("Cidade pequena");
+}
+
+function digaUmNome(nome) {
+    console.log(nome);
+}
+
+digaUmNome("Shal")
+
+function divisao(num1, num2){
+    const resultado = num1 / num2
+    console.log(resultado);
+}
+
+divisao(40, 8)
+
+console.log("2" + "2" - "2");
